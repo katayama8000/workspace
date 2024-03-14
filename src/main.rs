5046c04efa97ea5112462044af004ccc887b1012
katayama800 <tattu.0310@gmail.com>
2024-03-14 15:01:02 +0000

    init

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,3 @@
-#[macro_use]
-extern crate rocket;
-
-use rocket::{Build, Rocket};
-
-mod controller;
-mod database;
-mod models;
-mod schema;
-
-#[launch]
-fn rocket() -> Rocket<Build> {
-    rocket::build().mount(
-        "/",
-        routes![
-            controller::index,
-            controller::new_sighting,
-            controller::all_sightings,
-            controller::delete_sighting
-        ],
-    )
+fn main() {
+    println!("Hello, world");
 }
