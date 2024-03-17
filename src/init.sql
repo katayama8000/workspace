ae6fd54773cafb3f1695bf21c058fca20e47bdba
katayama800 <tattu.0310@gmail.com>
2024-03-17 12:48:59 +0000

    impl db

---
+++ src/init.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS Users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    gender VARCHAR(10)
+);
+
+CREATE TABLE IF NOT EXISTS MonthlyBills (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES Users(id),
+    total_amount DECIMAL(10, 2),
+    status VARCHAR(20),
+    month VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS BillDetails (
+    id SERIAL PRIMARY KEY,
+    bill_id INT REFERENCES MonthlyBills(id),
+    name VARCHAR(255) NOT NULL,
+    price DECIMAL(10, 2),
+    quantity INT
+);
+
+INSERT INTO
+    Users (name, email, gender)
+VALUES
+    ('John Doe', 'john.doe@example.com', 'Male');
+
+INSERT INTO
+    MonthlyBills (user_id, total_amount, status, month)
+VALUES
+    (1, 100, 'completed', 'June');
+
+INSERT INTO
+    BillDetails (bill_id, name, price, quantity)
+VALUES
+    (1, 'Electricity', 50, 1),
+    (1, 'Water', 30, 1),
+    (1, 'Internet', 20, 1);
--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,11 @@ struct AppState {
 
 async fn connect() -> Result<Pool<sqlx::Postgres>, sqlx::Error> {
     dotenv().ok();
-    let db_user = env::var("DB_USER").expect("DB_USER must be set in the .env file");
-    let db_password = env::var("DB_PASSWORD").expect("DB_PASSWORD must be set in the .env file");
-    let db_host = env::var("DB_HOST").expect("DB_HOST must be set in the .env file");
-    let db_name = env::var("DB_NAME").expect("DB_NAME must be set in the .env file");
+    let db_user = env::var("DB_USER_DEV").expect("DB_USER must be set in the .env file");
+    let db_password =
+        env::var("DB_PASSWORD_DEV").expect("DB_PASSWORD must be set in the .env file");
+    let db_host = env::var("DB_HOST_DEV").expect("DB_HOST must be set in the .env file");
+    let db_name = env::var("DB_NAME_DEV").expect("DB_NAME must be set in the .env file");
     let database_url = format!(
         "postgres://{}:{}@{}/{}?sslmode=require",
         db_user, db_password, db_host, db_name
@@ -34,8 +35,14 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     let state = AppState { pool };
 
     let app = Router::new()
-        .route("/", get(|| async { println!("hello") }))
-        .route("/items", get(handle_get_all_items))
+        .route(
+            "/",
+            get(|| async { (StatusCode::OK, "Hello, World from Axum!") }),
+        )
+        .route("/version", get(handle_get_version))
+        .route("/user", get(handle_get_user))
+        .route("/bill", get(handle_get_monthly_bill))
+        .route("/bill/details", get(handle_get_bill_details))
         .with_state(state);
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080")
         .await
@@ -46,24 +53,80 @@ async fn main() -> Result<(), Box<dyn error::Error>> {
     Ok(())
 }
 
-async fn handle_get_all_items(State(state): State<AppState>) -> impl IntoResponse {
-    println!("GET /");
-    let rows = match sqlx::query("SELECT * FROM playing_with_neon")
-        .fetch_all(&state.pool)
+async fn handle_get_version() -> String {
+    let version = env!("CARGO_PKG_VERSION");
+    format!("Version: {}", version)
+}
+
+async fn handle_get_user(State(state): State<AppState>) -> impl IntoResponse {
+    let email = "john.doe@example.com";
+    let row = match sqlx::query("SELECT * FROM users WHERE email = $1")
+        .bind(email)
+        .fetch_one(&state.pool)
         .await
     {
-        Ok(rows) => rows,
+        Ok(row) => row,
         Err(e) => {
-            eprintln!("Failed to fetch items: {:?}", e);
+            eprintln!("Failed to fetch user: {:?}", e);
             return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to fetch").into_response();
         }
     };
-    println!("Got {} items", rows.len());
-    for row in rows {
-        let id: i32 = row.get("id");
-        let name: String = row.get("name");
-        println!("Item {}: {}", id, name);
-    }
+
+    let id: i32 = row.get("id");
+    let name: String = row.get("name");
+    println!("User {}: {}", id, name);
+
+    (StatusCode::OK).into_response()
+}
+
+async fn handle_get_monthly_bill(State(state): State<AppState>) -> impl IntoResponse {
+    let month = "June";
+
+    let row = match sqlx::query("SELECT * FROM monthlybills WHERE month = $1")
+        .bind(month)
+        .fetch_one(&state.pool)
+        .await
+    {
+        Ok(row) => row,
+        Err(e) => {
+            eprintln!("Failed to fetch monthly bill: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to fetch").into_response();
+        }
+    };
+
+    let id: i32 = row.get("id");
+    // FIXME: This is not working
+    // let amount: f64 = row.get::<_, f64>("total_amount").to_f64().unwrap_or(0.0);
+    let status: String = row.get("status");
+
+    println!("Monthly bill {}: {}", id, status);
+
+    (StatusCode::OK).into_response()
+}
+
+async fn handle_get_bill_details(State(state): State<AppState>) -> impl IntoResponse {
+    let id = 1;
+    let bill_id = 1;
+
+    let row = match sqlx::query("SELECT * FROM billdetails WHERE id = $1 AND bill_id = $2")
+        .bind(id)
+        .bind(bill_id)
+        .fetch_one(&state.pool)
+        .await
+    {
+        Ok(row) => row,
+        Err(e) => {
+            eprintln!("Failed to fetch bill details: {:?}", e);
+            return (StatusCode::INTERNAL_SERVER_ERROR, "Failed to fetch").into_response();
+        }
+    };
+
+    let id: i32 = row.get("id");
+    let name: String = row.get("name");
+    // FIXME: This is not working
+    // let price: f64 = row.get("price");
+
+    println!("Monthly bill {}: {}", id, name);
 
     (StatusCode::OK).into_response()
 }
